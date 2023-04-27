88812d38f058aaa665ddb663583d57dee2710294
kebora <danielkebora@gmail.com>
2023-04-27 12:15:34 +0300

    config jog app

--- lib/modules/mobile_apps/jogging_app/jogging_app_view.dart
+++ lib/modules/mobile_apps/jogging_app/jogging_app_view.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portf/colors.dart';
 
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as js;
+
 const joggingAppDescription = '''
 I wanted to make a decision on whether to jog & the outfit to put on an hour earlier.\n
 This app consumes the weatherbit API for the data.
 ''';
+
+const String _githubLink = "https://github.com/kebora/jog";
+const String _playstoreLink =
+    "https://play.google.com/store/apps/details?id=com.kebora.boraweather";
 
 class JoggingAppView extends StatelessWidget {
   const JoggingAppView({super.key});
@@ -30,7 +37,7 @@ class JoggingAppView extends StatelessWidget {
                   child: Column(
                     children: [
                       const Text(
-                        "JOGGING APP",
+                        "Jogging App",
                         style: TextStyle(
                           fontSize: 50,
                           color: Colors.white,
@@ -51,11 +58,15 @@ class JoggingAppView extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              js.context.callMethod('open', [_githubLink]);
+                            },
                             icon: const Icon(FontAwesomeIcons.github),
                           ),
                           IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              js.context.callMethod('open', [_playstoreLink]);
+                            },
                             icon: const Icon(FontAwesomeIcons.googlePlay),
                           ),
                         ],
