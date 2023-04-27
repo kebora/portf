09d784363f96f487c5692fe5521184822e795927
kebora <danielkebora@gmail.com>
2023-04-27 12:21:47 +0300

    config gsd app

--- lib/modules/mobile_apps/gsd_app/gsd_app_view.dart
+++ lib/modules/mobile_apps/gsd_app/gsd_app_view.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portf/colors.dart';
 
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:js' as js;
+
 const gsdAppDescription = '''
  First interaction with flutter, thought it would be fun making a custom diary app.
 ''';
+const String _playstoreLink =
+    "https://play.google.com/store/apps/details?id=com.simiyu.greensundiary";
+const String _githubLink = "https://github.com/kebora/greensundiary";
 
 class GSDAppView extends StatelessWidget {
   const GSDAppView({super.key});
@@ -25,7 +31,7 @@ class GSDAppView extends StatelessWidget {
                   child: Column(
                     children: [
                       const Text(
-                        "GREEN SUN DIARY",
+                        "Green Sun Diary",
                         style: TextStyle(
                           fontSize: 50,
                           color: Colors.white,
@@ -55,11 +61,15 @@ class GSDAppView extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              js.context.callMethod('open', [_githubLink]);
+                            },
                             icon: const Icon(FontAwesomeIcons.github),
                           ),
                           IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              js.context.callMethod('open', [_playstoreLink]);
+                            },
                             icon: const Icon(FontAwesomeIcons.googlePlay),
                           ),
                         ],
